61cdf0b236b286d5c76f061b781fa4f2d697f168
JonnyKato <78195864+JohnnyKato@users.noreply.github.com>
2026-01-04 07:36:05 -0300

    merged visual plugins into theme.lua

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        'vague-theme/vague.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('vague').setup({
+                bold = false,
+                italic = false
+            })
+            vim.cmd.colorscheme('vague')
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            options = {
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' }
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {{ 'filename', path = 1 }},
+                lualine_x = {{ 'lsp_status', symbols = {done = ''}, separator = ' |' }, { 'encoding', separator = ' |' }, { 'fileformat', separator = ' |' }, 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+        },
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        ---@module 'ibl'
+        ---@type ibl.config
+        opts = {indent = {char = '│'}},
+    },
+}
